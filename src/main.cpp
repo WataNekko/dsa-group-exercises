fc073370dde3df7143f29589c1821ff93c17bacf
NguyenLe1605 <dangnguyenhentai@gmail.com>
2022-03-04 22:38:13 +0700

    First version of cli

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,34 @@
 #include "SortAlgorithm.h"
 #include "Random.h"
 #include <fstream>
+#include <iostream>
+#include <string>
+
 using std::endl;
 
 static const vector<int> TEST_SIZES{10, 50, 100, 200, 500, 1000, 2000, 5000, 7000, 10000,
                                     20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000}; 
 
-int main() {
-    std::ofstream stream("../res/result.csv");
-    if (!stream.is_open())
+int main(int argc, char* argv[]) {
+
+    if (argc > 2) {
+        std::cout << "Error! Program only accept one filename or no filename" << endl;
         return -1;
-    stream << "Size of input,Insertion Sort,Merge Sort,Quicksort" << endl;
+    }
+
+    std::ofstream file;
+    const char* filename = argv[1];
+
+    if (argc > 1) 
+        file.open(filename, std::ios_base::out | std::ios_base::trunc);
+
+    std::ostream& stream = (argc == 2) ? file : std::cout;
+
+    if (!stream.good()) {
+        std::cout << "Error with stream! Can not run the program" << endl;
+        return -1;
+    }
+    stream << "\"Size of input\",\"Insertion Sort\",\"Merge Sort\",\"Quicksort\"" << endl;
 
     for (auto size: TEST_SIZES) {
         auto test_array = GenerateSequenceOfSize<int>(size);
@@ -21,10 +39,8 @@ int main() {
         auto MergeSortTime = timer.SortingTiming<int>(MergeSort<int>, test_array);
         auto QuickSortTime = timer.SortingTiming<int>(QuickSort<int>, test_array);
 
-        stream << size << "," << InsertionTime << "ms," << MergeSortTime << "ms," << QuickSortTime << "ms" << endl;
+        stream << size << "," << InsertionTime << "," << MergeSortTime << "," << QuickSortTime << endl;
     }
-
-    stream.close();
 
     return 0;
 }

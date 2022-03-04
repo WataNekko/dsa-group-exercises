2b12f9e810a04fea398a8978694b254f7f3bfecf
NguyenLe1605 <dangnguyenhentai@gmail.com>
2022-03-04 19:58:03 +0700

    Fixing the stream bug

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,13 @@
 #include "SortAlgorithm.h"
 #include "Random.h"
 #include <fstream>
-
 using std::endl;
 
 static const vector<int> TEST_SIZES{10, 50, 100, 200, 500, 1000, 2000, 5000, 7000, 10000,
                                     20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000}; 
 
 int main() {
-    std::fstream stream("../res/result.csv");
+    std::ofstream stream("../res/result.csv");
     if (!stream.is_open())
         return -1;
     stream << "Size of input,Insertion Sort,Merge Sort,Quicksort" << endl;
@@ -24,6 +23,8 @@ int main() {
 
         stream << size << "," << InsertionTime << "ms," << MergeSortTime << "ms," << QuickSortTime << "ms" << endl;
     }
+
+    stream.close();
 
     return 0;
 }

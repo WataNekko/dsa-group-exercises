0aa7964b694a430aae84e0c50dd2cd7481f255da
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-03-07 10:16:54 +0700

    Edit logging string

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@ using std::vector;
 
 static const vector<int> TEST_SIZES{10, 50, 100, 200, 500, 1000, 2000, 5000, 7000, 10000,
                                     20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
-                                    
-int main(int argc, char* argv[]) {
+
+int main(int argc, char *argv[])
+{
 
     if (argc > 2) {
         std::cout << "Error! Program only accept one filename or no filename" << endl;
@@ -21,12 +22,12 @@ int main(int argc, char* argv[]) {
     }
 
     std::ofstream file;
-    const char* filename = argv[1];
+    const char *filename = argv[1];
 
-    if (argc > 1) 
+    if (argc > 1)
         file.open(filename, std::ios_base::out | std::ios_base::trunc);
 
-    std::ostream& stream = (argc == 2) ? file : std::cout;
+    std::ostream &stream = (argc == 2) ? file : std::cout;
 
     if (!stream.good()) {
         std::cout << "Error with stream! Can not run the program" << endl;
@@ -38,15 +39,14 @@ int main(int argc, char* argv[]) {
 
     stream << "\"Size of input\",\"Insertion Sort\",\"Merge Sort\",\"Quicksort\"" << endl;
 
-    for (auto size: TEST_SIZES) {
-        int* TestArray = new int[size];
+    for (auto size : TEST_SIZES) {
+        int *TestArray = new int[size];
         Timer timer;
 
         GenerateRandomArray(TestArray, size);
         auto InsertionTime = timer.SortingTiming(InsertionSort, TestArray, size);
         stream << size << ',' << InsertionTime << std::flush;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
 
         GenerateRandomArray(TestArray, size);
         auto MergeTime = timer.SortingTiming(MergeSort, TestArray, size);
@@ -61,7 +61,7 @@ int main(int argc, char* argv[]) {
         delete TestArray;
     }
 
-    std::cout << "Done benchmark." << endl;
+    std::cout << "Done benchmark" << endl;
 
     return 0;
 }

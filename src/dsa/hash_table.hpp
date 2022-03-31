52b9a73f3472cf1dffb2833f0e5046a948ef8d47
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-03-31 14:39:49 +0700

    Add hash_table.hpp

--- src/dsa/hash_table.hpp
+++ src/dsa/hash_table.hpp
@@ -1,8 +1,30 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include "linked_list.hpp"
+
 namespace dsa {
 
+    template <typename T>
     class HashTable {
+    private:
+        constexpr size_t SIZE = 100;
+        std::vector<LinkedList<T>> buckets;
+
+        size_t hash(const char *cstr)
+        {
+            return 0;
+        }
+
+    public:
+        HashTable() : buckets(SIZE) {}
+
+        T &operator[](const char *cstr)
+        {
+            return;
+        }
     };
 
 } // namespace dsa

e584a716f563d54cf1c605a61425d86bfbf63a4d
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-04-21 18:53:58 +0700

    Update HashTable

--- src/dsa/hash_table.hpp
+++ src/dsa/hash_table.hpp
@@ -1,30 +1,145 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
 #include <vector>
 
+#include "hash.hpp"
 #include "linked_list.hpp"
 
 namespace dsa {
 
     template <typename T>
-    class HashTable {
-    private:
-        constexpr size_t SIZE = 100;
-        std::vector<LinkedList<T>> buckets;
+    struct KeyValuePair {
+        const std::string key;
+        T value;
+    };
 
-        size_t hash(const char *cstr)
+    template <typename T, size_t BucketCount = 100>
+    class HashTable {
+    public:
+        using Entry = KeyValuePair<T>;
+
+        HashTable() : buckets_(BucketCount) {}
+
+        template <size_t Size>
+        HashTable(Entry(&&entries)[Size]) : buckets_(BucketCount)
         {
-            return 0;
+            for (auto &entry : entries) {
+                insert(std::move(entry));
+            }
+        }
+
+    private:
+        void insert(Entry &&entry)
+        {
+            auto &bucket = getBucket(entry.key);
+
+            for (auto &e : bucket) {
+                if (e.key == entry.key) {
+                    // key already exists
+                    e.value = std::move(entry.value);
+                    return;
+                }
+            }
+
+            // insert new entry
+            bucket.insert(std::move(entry));
+        }
+
+        template <typename K>
+        inline LinkedList<Entry> &getBucket(const K &key)
+        {
+            return buckets_[dsa::hash(key, BucketCount)];
+        }
+
+        template <typename K>
+        inline const LinkedList<Entry> &getBucket(const K &key) const
+        {
+            return buckets_[dsa::hash(key, BucketCount)];
         }
 
     public:
-        HashTable() : buckets(SIZE) {}
-
-        T &operator[](const char *cstr)
+        template <typename K>
+        T &operator[](K &&key)
         {
-            return;
+            auto &bucket = getBucket(key);
+
+            for (auto &entry : bucket) {
+                // find entry with same key
+                if (entry.key == key) {
+                    return entry.value;
+                }
+            }
+
+            // key not found. insert new entry
+            auto &entry = bucket.insert(Entry{std::forward<K>(key), T()});
+            return entry.value;
         }
-    };
+
+        T &operator[](const char *const key)
+        {
+            auto &bucket = getBucket(key);
+
+            for (auto &entry : bucket) {
+                // find entry with same key
+                if (entry.key == key) {
+                    return entry.value;
+                }
+            }
+
+            // key not found. insert new entry
+            auto &entry = bucket.insert(Entry{key, T()});
+            return entry.value;
+        }
+
+        template <typename K>
+        const T &operator[](const K &key) const
+        {
+            auto &bucket = getBucket(key);
+
+            for (auto &entry : bucket) {
+                // find entry with same key
+                if (entry.key == key) {
+                    return entry.value;
+                }
+            }
+
+            // key not found. throw exception
+            throw std::out_of_range("Key not found");
+        }
+
+        const T &operator[](const char *const key) const
+        {
+            auto &bucket = getBucket(key);
+
+            for (auto &entry : bucket) {
+                // find entry with same key
+                if (entry.key == key) {
+                    return entry.value;
+                }
+            }
+
+            // key not found. throw exception
+            throw std::out_of_range("Key not found");
+        }
+
+        template <typename K>
+        inline bool remove(const K &key)
+        {
+            return getBucket(key)
+                .removeIf([&](const Entry &entry) { return entry.key == key; });
+        }
+
+        inline bool remove(const char *const key)
+        {
+            return getBucket(key)
+                .removeIf([&](const Entry &entry) { return entry.key == key; });
+        }
+
+    private:
+        std::vector<LinkedList<Entry>> buckets_;
+
+    }; // class HashTable
 
 } // namespace dsa

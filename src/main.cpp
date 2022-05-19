e37f844be57cd00df0d553b36bc60be2b0c9ac64
Nam <18770@student.vgu.edu.vn>
2022-05-19 15:59:08 +0700

    Update :((((((

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,35 @@
 
 #include "dsa/dijkstra_algo.hpp"
 
-enum class Vertex {
-    A,
-    B,
-    C,
-    D,
-    E
+enum class Vertex : char
+{
+    S,
+    T,
+    X,
+    Y,
+    Z
 };
 
 std::ostream &operator<<(std::ostream &os, const Vertex &vertex)
 {
-    return os << static_cast<char>(static_cast<char>(vertex) + 'A');
+    return os << ([&]()
+                  {
+        switch (vertex) {
+        case Vertex::S:
+            return 'S';
+            break;
+        case Vertex::T:
+            return 'T';
+            break;
+        case Vertex::X:
+            return 'X';
+            break;
+        case Vertex::Y:
+            return 'Y';
+            break;
+        default:
+            return 'Z';
+    } })();
 }
 
 template <typename Vertex, typename Weight>
@@ -29,14 +47,14 @@ int main()
     constexpr size_t VertexCount = 5;
     constexpr int Inf = std::numeric_limits<int>::max();
 
-    Vertex start = Vertex::A;
+    Vertex start = Vertex::S;
 
     int distances[VertexCount][VertexCount] =
-        {{0, 6, Inf, 1, Inf},
-         {6, 0, 5, 2, 2},
-         {Inf, 5, 0, Inf, 5},
-         {1, 2, Inf, 0, 1},
-         {Inf, 2, 5, 1, 0}};
+        {{0, 10, Inf, 5, Inf},
+         {Inf, 0, 1, 2, Inf},
+         {Inf, Inf, 0, Inf, 4},
+         {Inf, 8, 9, 0, 2},
+         {7, Inf, 6, Inf, 0}};
 
     dsa::ShortestPathInfo<Vertex, int> result[VertexCount];
 
@@ -47,26 +65,30 @@ int main()
     // display result
 
     std::cout << "Dijkstra's algorithm result:\n\n";
-    for (size_t i = 0; i < VertexCount; i++) {
+    for (size_t i = 0; i < VertexCount; i++)
+    {
         std::cout << "vertex: " << static_cast<Vertex>(i) << ", " << result[i] << '\n';
     }
     std::cout << '\n';
 
     std::cout << "Shortest path from " << start << '\n';
-    for (size_t i = 0; i < VertexCount; i++) {
+    for (size_t i = 0; i < VertexCount; i++)
+    {
         std::stack<Vertex> path;
 
         path.push(static_cast<Vertex>(i));
-        while (path.top() != result[static_cast<size_t>(path.top())].previousVertex) {
+        while (path.top() != result[static_cast<size_t>(path.top())].previousVertex)
+        {
             path.push(result[static_cast<size_t>(path.top())].previousVertex);
         }
 
         std::cout << "To " << static_cast<Vertex>(i) << ": " << path.top();
         path.pop();
-        while (!path.empty()) {
+        while (!path.empty())
+        {
             std::cout << " -> " << path.top();
             path.pop();
         }
-        std::cout << '\n';
+        std::cout << " (cost: " << result[i].shortestDistance << ")\n";
     }
 }
